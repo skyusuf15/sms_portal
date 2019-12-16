d1610dfe2f598dca5b3b13458b50ca6125dfd131
Sanusi Yusuf K <sanusikorede@gmail.com>
2019-12-16 09:15:49 +0100

    minor bug fix

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -105,7 +105,7 @@
                 <h3 class="box-title">SMS Chart</h3>
                 <div class="box-tools pull-right">
                   <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-                  {{-- <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button> --}}
+                  <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button> 
                 </div>
             </div>
             <div class="box-body" style="">
